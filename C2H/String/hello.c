28816cf82f5774f7db17004daf66b13a03d4ba16
madjestic <madjestic13@gmail.com>
2016-12-10 03:08:36 +0100

    initial commit

---
+++ C2H/String/hello.c
@@ -0,0 +1,14 @@
+// #include<stdio.h>
+
+const char * s_var = "abcstring";
+
+// const char * getString(const char *x)
+// {
+//   return x;
+// }
+
+// main()
+// {
+//   const char *y = getString(x);
+//   printf("%s\n", y);
+// }
---
+++ C2H/String/s_var.c
@@ -0,0 +1,7 @@
+const char * s_var = "abc";
+
+const char* abc()
+{
+  const char * s_var = "suka";
+  return s_var;
+}
